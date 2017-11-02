5cf6f03d94788da5ca85c7609532757b2bd8e0b4
EllangoK <ellangok@bxscience.edu>
2017-11-01 21:59:55 -0400

    Moved the JS

--- js/FirebaseHandler.js
+++ js/FirebaseHandler.js
@@ -1,33 +1,26 @@
-(function(){
-  var newscript = document.createElement('script');
-     newscript.type = 'text/javascript';
-     newscript.async = true;
-     newscript.src = 'https://www.gstatic.com/firebasejs/3.0.2/firebase.js';
-  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
-})();
-
-_setFormData = function setFormData (sel, data) {
-  console.info('setting form to data', data);
-  var inputList = document.querySelectorAll(sel + ' [name]');
-  [].forEach.call(inputList, function(input) {
-      console.log(input);
-      if (data[input.name] && data[input.name] !== "undefined") {
-        input.value = data[input.name];
-      }
+$.fn.getFormData = function() {
+  var fields = this.find('[name]');
+  var result = {};
+  $.each(fields, function (i, el) {
+    result[el.name] = el.value;
   });
+  return result;
 };
-var _fb;
-var fbToForm = function fbToForm (key, sel) {
-  var config = config || {
-    apiKey: "AIzaSyCmxZbtAWv6JqgM2NqtSqQsm67BFcHoPfY",
-    authDomain: "sciborgs-scouting-app.firebaseapp.com",
-    databaseURL: "https://sciborgs-scouting-app.firebaseio.com",
-    projectId: "sciborgs-scouting-app",
-    storageBucket: "sciborgs-scouting-app.appspot.com",
-    messagingSenderId: "372162650684"
-  };
-    _fb = _fb && _fb.name === "fbToForm" ? _fb : firebase.initializeApp(config, "fbToForm");
-    _fb.database().ref('user-data/' + key).on('value', function(snapshot) {
-        _setFormData(sel, snapshot.val());
-    });
+var config = {
+apiKey: "AIzaSyCmxZbtAWv6JqgM2NqtSqQsm67BFcHoPfY",
+authDomain: "sciborgs-scouting-app.firebaseapp.com",
+databaseURL: "https://sciborgs-scouting-app.firebaseio.com",
+projectId: "sciborgs-scouting-app",
+storageBucket: "sciborgs-scouting-app.appspot.com",
+messagingSenderId: "372162650684"
 };
+  var fb = firebase.initializeApp(config);
+  var user_id;
+  $('.submit').on('click', function (e) {
+    e.preventDefault();
+    var updates = {};
+    user_id = user_id ? user_id : fb.database().ref().child('user_data').push().key;
+    updates[user_id] = $('form').getFormData();
+    var path = 'Competitions/' + document.getElementById("activityCatagory").value + '/Teams/' + document.getElementById("teamnumber").value;
+    fb.database().ref().child(path).update(updates);
+  });
