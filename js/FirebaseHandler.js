f34658bb7e8fafd941d4902f5c04b809e2171233
EllangoK <ellangok@bxscience.edu>
2017-11-05 21:13:08 -0500

    Rewrote the forms in bootstrap and better input validation

--- js/FirebaseHandler.js
+++ js/FirebaseHandler.js
@@ -36,7 +36,11 @@ messagingSenderId: "372162650684"
     var path = 'Competitions/' + document.getElementById("activityCatagory").value + '/Teams/' + document.getElementById("teamnumber").value;
     fb.database().ref().child(path).update(updates);
   });
-  function ValidateNumberOnly() {
-    if ((event.keyCode < 48 || event.keyCode > 57)) {
-     event.returnValue = false;
-  }}
+  function isNumber(evt) {
+        evt = (evt) ? evt : window.event;
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if ( (charCode > 31 && charCode < 48) || charCode > 57) {
+            return false;
+        }
+        return true;
+    }
