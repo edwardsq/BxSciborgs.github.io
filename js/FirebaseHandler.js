0f24cf709b8c712f9dc1655c3eb41d2cc35abb36
EllangoK <ellangok@bxscience.edu>
2017-11-05 21:15:37 -0500

    Better? Input validation

--- js/FirebaseHandler.js
+++ js/FirebaseHandler.js
@@ -36,11 +36,9 @@ messagingSenderId: "372162650684"
     var path = 'Competitions/' + document.getElementById("activityCatagory").value + '/Teams/' + document.getElementById("teamnumber").value;
     fb.database().ref().child(path).update(updates);
   });
-  function isNumber(evt) {
-        evt = (evt) ? evt : window.event;
-        var charCode = (evt.which) ? evt.which : evt.keyCode;
-        if ( (charCode > 31 && charCode < 48) || charCode > 57) {
-            return false;
-        }
-        return true;
-    }
+  function isNumberKey(evt){
+      var charCode = (evt.which) ? evt.which : event.keyCode
+      if (charCode > 31 && (charCode < 48 || charCode > 57))
+          return false;
+      return true;
+  }
