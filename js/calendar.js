786b21258af4dd1b379fbe732188b640466d47cc
EllangoK <ellangok@bxscience.edu>
2017-11-16 18:41:43 -0500

    Fixed the Calendar and made it look nice

--- js/calendar.js
+++ js/calendar.js
@@ -8,10 +8,13 @@ $('#calendar').fullCalendar({
     eventSources: [{
         googleCalendarApiKey: 'AIzaSyA1EmSAhtmmc76bCegYKOfb-ShUFo-kgJg',
         googleCalendarId: 'bxscience.edu_orov21k09sm1sq0558q6julsbc@group.calendar.google.com',        
-        className: 'fc-event-email'
-    }],
+        className: 'fc-event-email',
+        color: 'rgb(68, 68, 68)',
+        textColor: "#FAEC29"
+    }], 
     selectable: true,
     selectHelper: true,
-    editable: false  
+    editable: false,
+    themeSystem: 'bootstrap3'
 });
 });
